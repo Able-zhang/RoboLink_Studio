527f73139eaa0f35eb9ec13c022e1c40f01203d7
Able-zhang <zjb384490460@gmail.com>
2026-06-09 22:50:18 +0800

    添加 .gitignore 并清理编译产物
    - 添加完整的 .gitignore 文件（C++, Qt, CMake, Visual Studio）
    - 从版本控制中删除 build/ 目录
    - 从版本控制中删除 .qtcreator/*.user 文件
    - 清理所有不应提交的编译和IDE配置文件

--- build/Desktop_Qt_6_9_3_MSVC2022_64bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_RoboLinkStudio_FILE E:/RoboLink/RoboLinkStudio/RoboLinkStudio/build/Desktop_Qt_6_9_3_MSVC2022_64bit-Debug/bin/RoboLinkStudio.exe)
-set(__QT_DEPLOY_TARGET_RoboLinkStudio_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_RoboLinkStudio_RUNTIME_DLLS D:/Qt_Creater/6.9.3/msvc2022_64/bin/Qt6Widgetsd.dll;D:/Qt_Creater/6.9.3/msvc2022_64/bin/Qt6Guid.dll;D:/Qt_Creater/6.9.3/msvc2022_64/bin/Qt6Cored.dll)
--- build/Desktop_Qt_6_9_3_MSVC2022_64bit-Debug/.qt/deploy_RoboLinkStudio_bc75c63376.cmake
+++
@@ -1,8 +0,0 @@
-include("E:/RoboLink/RoboLinkStudio/RoboLinkStudio/build/Desktop_Qt_6_9_3_MSVC2022_64bit-Debug/.qt/QtDeploySupport.cmake")
-include("${CMAKE_CURRENT_LIST_DIR}/RoboLinkStudio-plugins.cmake" OPTIONAL)
-set(__QT_DEPLOY_I18N_CATALOGS "qtbase")
-
-qt6_deploy_runtime_dependencies(
-    EXECUTABLE "E:/RoboLink/RoboLinkStudio/RoboLinkStudio/build/Desktop_Qt_6_9_3_MSVC2022_64bit-Debug/bin/RoboLinkStudio.exe"
-    GENERATE_QT_CONF
-)
